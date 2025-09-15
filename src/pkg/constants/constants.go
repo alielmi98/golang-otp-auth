f63514a22fc19ccce9be1b2bd96d0612e29fc2eb
Ali Elmi <ali.elmi77@yahoo.com>
2025-09-15 22:06:25 +0330

    feat: Add Redis and PostgreSQL configuration with Docker support
    - Implemented Redis caching with connection management in `src/pkg/cache/redis.go`.
    - Added PostgreSQL database connection handling in `src/pkg/db/postgres.go`.
    - Created configuration files for development, production, and Docker environments in `src/pkg/config/`.
    - Introduced error handling and status code mapping for service errors in `src/pkg/service_errors/`.
    - Set up VSCode launch and task configurations for Go development.
    - Added Docker Compose setup for PostgreSQL and Redis services.
    - Defined constants for logging and error handling in `src/pkg/constants/`.
    - Enhanced base response structure for API responses in `src/pkg/helper/`.

---
+++ src/pkg/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+const (
+	// User
+	AdminRoleName   string = "admin"
+	DefaultRoleName string = "default"
+	DefaultUserName string = "admin"
+	// Claims
+	AuthorizationHeaderKey string = "Authorization"
+	MobileNumberKey        string = "MobileNumber"
+	ExpireTimeKey          string = "Exp"
+
+	RefreshTokenCookieName string = "refresh_token"
+)
---
+++ src/pkg/db/postgres.go
@@ -0,0 +1,60 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/alielmi98/golang-otp-auth/pkg/config"
+	"github.com/alielmi98/golang-otp-auth/pkg/constants"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var dbClient *gorm.DB
+
+func InitDb(cfg *config.Config) error {
+	var err error
+	cnn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Tehran",
+		cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.User, cfg.Postgres.Password,
+		cfg.Postgres.DbName, cfg.Postgres.SSLMode)
+
+	dbClient, err = gorm.Open(postgres.Open(cnn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	sqlDb, _ := dbClient.DB()
+	err = sqlDb.Ping()
+	if err != nil {
+		return err
+	}
+
+	sqlDb.SetMaxIdleConns(cfg.Postgres.MaxIdleConns)
+	sqlDb.SetMaxOpenConns(cfg.Postgres.MaxOpenConns)
+	sqlDb.SetConnMaxLifetime(cfg.Postgres.ConnMaxLifetime * time.Minute)
+
+	log.Printf("caller:%s  Level:%s Msg:Db connection established", constants.Postgres, constants.Startup)
+	return nil
+}
+
+func GetDb() *gorm.DB {
+	return dbClient
+}
+
+func CloseDb() {
+	con, _ := dbClient.DB()
+	con.Close()
+}
+
+type PreloadEntity struct {
+	Entity string
+}
+
+// Preload
+func Preload(db *gorm.DB, preloads []PreloadEntity) *gorm.DB {
+	for _, item := range preloads {
+		db = db.Preload(item.Entity)
+	}
+	return db
+}
